7f70c11b6c2001a5dc9ce83c5a3c603d01c31b44
Ricky Ho <horicky78@gmail.com>
2021-07-26 17:43:10 +0000

    Sign in and redirect

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,4 +1,14 @@
 const signInButton = document.querySelector('.button');
 signInButton.addEventListener("click", () => {
     console.log('sign in clicked');
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+        console.log("login success", user);
+        window.location = "writeNote.html";
+    })
+    .catch(error => {
+        console.log("login failed", error);
+    })
 })
+
